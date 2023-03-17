50b5dbb35031ebbc96ac1945707d4998fcfc0e5d
blatsios <blatsios@yahoo.gr>
2023-03-17 14:07:41 +0200

    Updated SplitPdfWithAFM to check AFMs that contain spaces

--- Payroll tools/SplitPdfWithAFM/main.py
+++ Payroll tools/SplitPdfWithAFM/main.py
@@ -14,28 +14,29 @@ class GUI:
         self.window.resizable(False, False)
         self.create_widgets()
 
-    def getOutputDirName(self):
-        dName = filedialog.askdirectory(initialdir="./data/", title="Επιλέξτε τον φάκελο που θα αποθηκευτούν τα αρχεία")
+    def get_output_dir_name(self):
+        d_name = filedialog.askdirectory(initialdir="./data/",
+                                         title="Επιλέξτε τον φάκελο που θα αποθηκευτούν τα αρχεία")
 
-        if dName == "":
+        if d_name == "":
             return
 
-        self.outputDirName.set(dName)
-        self.btnRun.configure(state='normal')
+        self.output_dir_name.set(d_name)
+        self.btn_run.configure(state='normal')
 
-    def getDataFilename(self):
-        fName = filedialog.askopenfilename(initialdir="./data/",
-                                           title="Επιλέξτε το αρχείο pdf",
-                                           filetypes=(("pdf files", "*.pdf"), ("all files", "*.*")))
+    def get_data_filename(self):
+        f_name = filedialog.askopenfilename(initialdir="./data/",
+                                            title="Επιλέξτε το αρχείο pdf",
+                                            filetypes=(("pdf files", "*.pdf"), ("all files", "*.*")))
 
-        if fName == "":
+        if f_name == "":
             return
 
-        self.dataFilename.set(fName)
-        self.ntrOutputDirName.configure(state='readonly')
-        self.btnOpenOutputDir.configure(state='normal')
+        self.data_filename.set(f_name)
+        self.ntr_output_dir_name.configure(state='readonly')
+        self.btn_open_output_dir.configure(state='normal')
 
-    def removeSpaces(self, text_list):
+    def remove_spaces(self, text_list):
         while '' in text_list:
             text_list.remove('')
 
@@ -69,35 +70,62 @@ class GUI:
         else:
             return False
 
-    def findAFM(self, l):
-        afmList = list()
+    def find_afm(self, text_list):
+        afm_list = list()
 
-        for item in l:
+        for item in text_list:
             if self.verify_afm(item) and item != '099709779':
-                afmList.append(item)
+                afm_list.append(item)
 
-        if afmList:
-            return afmList[-1]
+        if afm_list:
+            return afm_list[0]
+        else:
+            return self.find_afm_special(text_list)
+
+    def find_afm_special(self, text_list):
+        afm_list = list()
+
+        for i, item in enumerate(text_list):
+            test_afm = ''
+            if str(item).isdecimal():
+                if i + 8 <= len(text_list):
+                    for j in range(9):
+                        test_afm += text_list[i + j]
+
+                    if not test_afm.isdecimal():
+                        continue
+                else:
+                    break
+            else:
+                continue
+
+            if self.verify_afm(test_afm) and test_afm != '099709779':
+                afm_list.append(test_afm)
+
+        if afm_list:
+            return afm_list[0]
         else:
             return self.last_afm
 
     def run(self):
-        self.btnRun.configure(state='disabled')
+        self.btn_run.configure(state='disabled')
 
-        filename = self.dataFilename.get()
-        outputDir = self.outputDirName.get()
+        filename = self.data_filename.get()
+        output_dir = self.output_dir_name.get()
 
         data = dict()
 
         pdf = fitz.open(filename)
         self.last_afm = '000000000'
         for page in range(pdf.page_count):
-            pageObj = pdf[page]
-            pageText = pageObj.get_text("text")
-            textList = self.removeSpaces(pageText.replace('\n', ' ').split(' '))
+            page_obj = pdf[page]
+            page_text = page_obj.get_text("text")
+            text_list = self.remove_spaces(page_text.replace('\n', ' ').split(' '))
 
-            afm = self.findAFM(textList)
+            afm = self.find_afm(text_list)
             self.last_afm = afm
+
+            print(page, afm)
             if afm not in data:
                 data[afm] = list()
             data[afm].append(page)
@@ -108,7 +136,7 @@ class GUI:
             for page in data[afm]:
                 pdf_out.insert_pdf(pdf, from_page=page, to_page=page)
 
-            output_pdf = os.path.join(outputDir, afm + ".pdf")
+            output_pdf = os.path.join(output_dir, afm + ".pdf")
             pdf_out.save(output_pdf)
             pdf_out.close()
 
@@ -117,33 +145,33 @@ class GUI:
         showinfo(title="Ολοκλήρωση εκτέλεσης", message="Ο διαχωρισμός ολοκληρώθηκε.")
 
     def create_widgets(self):
-        self.fData = Frame(self.window)
+        self.f_data = Frame(self.window)
 
-        self.lData = Label(self.fData, text="Αρχείο:")
-        self.lData.grid(column=0, row=0, padx=10, pady=10, sticky=E)
+        self.l_data = Label(self.f_data, text="Αρχείο:")
+        self.l_data.grid(column=0, row=0, padx=10, pady=10, sticky=E)
 
-        self.dataFilename = StringVar()
-        self.ntrDataFilename = Entry(self.fData, width=128, state='readonly', textvariable=self.dataFilename)
-        self.ntrDataFilename.grid(column=1, row=0, padx=10, pady=10, sticky=W)
+        self.data_filename = StringVar()
+        self.ntr_data_filename = Entry(self.f_data, width=128, state='readonly', textvariable=self.data_filename)
+        self.ntr_data_filename.grid(column=1, row=0, padx=10, pady=10, sticky=W)
 
-        self.btnOpenData = Button(self.fData, text="Επιλέξτε αρχείο...", command=self.getDataFilename)
-        self.btnOpenData.grid(column=2, row=0, padx=10, pady=10)
+        self.btn_open_data = Button(self.f_data, text="Επιλέξτε αρχείο...", command=self.get_data_filename)
+        self.btn_open_data.grid(column=2, row=0, padx=10, pady=10)
 
-        self.lOutputDirName = Label(self.fData, text="Φάκελος για αποθήκευση των αρχείων:")
-        self.lOutputDirName.grid(column=0, row=1, padx=10, pady=10, sticky=E)
+        self.l_output_dir_name = Label(self.f_data, text="Φάκελος για αποθήκευση των αρχείων:")
+        self.l_output_dir_name.grid(column=0, row=1, padx=10, pady=10, sticky=E)
 
-        self.outputDirName = StringVar()
-        self.ntrOutputDirName = Entry(self.fData, width=128, state='disabled', textvariable=self.outputDirName)
-        self.ntrOutputDirName.grid(column=1, row=1, padx=10, pady=10, sticky=W)
+        self.output_dir_name = StringVar()
+        self.ntr_output_dir_name = Entry(self.f_data, width=128, state='disabled', textvariable=self.output_dir_name)
+        self.ntr_output_dir_name.grid(column=1, row=1, padx=10, pady=10, sticky=W)
 
-        self.btnOpenOutputDir = Button(self.fData, text="Επιλέξτε φάκελο...", command=self.getOutputDirName,
-                                       state='disabled')
-        self.btnOpenOutputDir.grid(column=2, row=1, padx=10, pady=10)
+        self.btn_open_output_dir = Button(self.f_data, text="Επιλέξτε φάκελο...", command=self.get_output_dir_name,
+                                          state='disabled')
+        self.btn_open_output_dir.grid(column=2, row=1, padx=10, pady=10)
 
-        self.btnRun = Button(self.fData, text="Εκτέλεση διαχωρισμού", command=self.run, state='disabled')
-        self.btnRun.grid(column=1, row=10, padx=10, pady=10)
+        self.btn_run = Button(self.f_data, text="Εκτέλεση διαχωρισμού", command=self.run, state='disabled')
+        self.btn_run.grid(column=1, row=10, padx=10, pady=10)
 
-        self.fData.pack()
+        self.f_data.pack()
 
 
 gui = GUI()
